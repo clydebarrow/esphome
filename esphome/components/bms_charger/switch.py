e479b8a124611be0fae1eb3ac86bf875034ab3e5
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-03-21 22:00:56 +1100

    Simplify switch schema

--- esphome/components/bms_charger/switch.py
+++ esphome/components/bms_charger/switch.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import (
-    CONF_TYPE,
+    DEVICE_CLASS_EMPTY,
+    ENTITY_CATEGORY_CONFIG,
 )
 from esphome.components import switch
 
@@ -14,7 +15,6 @@ from . import (
 BmsSwitch = charger.class_("BmsSwitch", switch.Switch, cg.Component)
 SwitchType = charger.enum("SwitchType")
 
-
 SWITCHES = {
     "full_charge": SwitchType.SW_FULL_CHARGE,
     "force_charge": SwitchType.SW_FORCE_CHARGE_1,
@@ -24,29 +24,23 @@ SWITCHES = {
 }
 
 
-def switch_schema(name):
-    return (
-        cv.Optional(name),
-        cv.Required(CONF_TYPE),
-        switch.switch_schema(BmsSwitch),
-    )
-
-
-CONFIG_SCHEMA = (
-    switch.switch_schema(BmsSwitch)
-    .extend(
-        {
-            cv.Required(CONF_TYPE): cv.enum(SWITCHES),
-            cv.GenerateID(CONF_CHARGER_ID): cv.use_id(ChargerComponent),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
+SWITCH_SCHEMA = switch.switch_schema(
+    BmsSwitch,
+    device_class=DEVICE_CLASS_EMPTY,
+    entity_category=ENTITY_CATEGORY_CONFIG,
+    default_restore_mode="RESTORE_DEFAULT_OFF",
 )
+
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
+    {
+        cv.GenerateID(CONF_CHARGER_ID): cv.use_id(ChargerComponent),
+    }
+).extend({cv.Optional(name): SWITCH_SCHEMA for name in SWITCHES})
 
 
 async def to_code(config):
     component = await cg.get_variable(config[CONF_CHARGER_ID])
-    type = config[CONF_TYPE]
-    if type in SWITCHES:
-        var = await switch.new_switch(config)
-        cg.add(component.add_switch(type, var))
+    for type in SWITCHES:
+        if swconf := config.get(type):
+            var = await switch.new_switch(swconf)
+            cg.add(component.add_switch(SWITCHES[type], var))
