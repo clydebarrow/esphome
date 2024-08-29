48f5fae9f87dfb291e0a4a8a6812f72464afc072
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-08-29 10:32:00 +1000

    WIP

--- esphome/components/rpi_dpi_rgb/rpi_dpi_rgb.cpp
+++ esphome/components/rpi_dpi_rgb/rpi_dpi_rgb.cpp
@@ -2,11 +2,16 @@
 #include "rpi_dpi_rgb.h"
 #include "esphome/core/log.h"
 
+#ifdef USE_OTA
+#include "esphome/components/ota/ota_backend.h"
+#endif
+
 namespace esphome {
 namespace rpi_dpi_rgb {
 
 void RpiDpiRgb::setup() {
-  esph_log_config(TAG, "Setting up RPI_DPI_RGB");
+  ESP_LOGCONFIG(TAG, "Setting up RPI_DPI_RGB");
+  this->reset_display_();
   esp_lcd_rgb_panel_config_t config{};
   config.flags.fb_in_psram = 1;
   config.timings.h_res = this->width_;
@@ -20,7 +25,6 @@ void RpiDpiRgb::setup() {
   config.timings.flags.pclk_active_neg = this->pclk_inverted_;
   config.timings.pclk_hz = this->pclk_frequency_;
   config.clk_src = LCD_CLK_SRC_PLL160M;
-  config.sram_trans_align = 64;
   config.psram_trans_align = 64;
   size_t data_pin_count = sizeof(this->data_pins_) / sizeof(this->data_pins_[0]);
   for (size_t i = 0; i != data_pin_count; i++) {
@@ -34,11 +38,30 @@ void RpiDpiRgb::setup() {
   config.pclk_gpio_num = this->pclk_pin_->get_pin();
   esp_err_t err = esp_lcd_new_rgb_panel(&config, &this->handle_);
   if (err != ESP_OK) {
-    esph_log_e(TAG, "lcd_new_rgb_panel failed: %s", esp_err_to_name(err));
+    ESP_LOGE(TAG, "lcd_new_rgb_panel failed: %s", esp_err_to_name(err));
+    this->mark_failed();
+    return;
   }
   ESP_ERROR_CHECK(esp_lcd_panel_reset(this->handle_));
   ESP_ERROR_CHECK(esp_lcd_panel_init(this->handle_));
-  esph_log_config(TAG, "RPI_DPI_RGB setup complete");
+#ifdef USE_OTA
+  ota::get_global_ota_callback()->add_on_state_callback(
+      [this](ota::OTAState state, float progress, uint8_t error, ota::OTAComponent *comp) {
+        auto pclk = this->pclk_frequency_;
+        ESP_LOGD(TAG, "OTA state changed: %d, progress: %f, error: %d", state, progress, error);
+        if (state == ota::OTA_ERROR || state == ota::OTA_ABORT) {
+          this->reset_display_();
+          ESP_ERROR_CHECK(esp_lcd_rgb_panel_set_pclk(this->handle_, pclk));
+        } else if (state == ota::OTA_STARTED) {
+          ESP_ERROR_CHECK(esp_lcd_rgb_panel_set_pclk(this->handle_, pclk / 4));
+        }
+      });
+#endif
+  ESP_LOGCONFIG(TAG, "RPI_DPI_RGB setup complete");
+}
+void RpiDpiRgb::loop() {
+  if (this->handle_ != nullptr)
+    esp_lcd_rgb_panel_restart(this->handle_);
 }
 
 void RpiDpiRgb::draw_pixels_at(int x_start, int y_start, int w, int h, const uint8_t *ptr, display::ColorOrder order,
@@ -64,12 +87,12 @@ void RpiDpiRgb::draw_pixels_at(int x_start, int y_start, int w, int h, const uin
     for (int y = 0; y != h; y++) {
       err = esp_lcd_panel_draw_bitmap(this->handle_, x_start, y + y_start, x_start + w, y + y_start + 1,
                                       ptr + ((y + y_offset) * stride + x_offset) * 2);
-      if (err != ESP_OK)
+      if (err != ESP_OK) {
+        ESP_LOGE(TAG, "lcd_lcd_panel_draw_bitmap failed: %s", esp_err_to_name(err));
         break;
+      }
     }
   }
-  if (err != ESP_OK)
-    esph_log_e(TAG, "lcd_lcd_panel_draw_bitmap failed: %s", esp_err_to_name(err));
 }
 
 void RpiDpiRgb::draw_pixel_at(int x, int y, Color color) {
@@ -109,7 +132,22 @@ void RpiDpiRgb::dump_config() {
   for (size_t i = 0; i != data_pin_count; i++)
     ESP_LOGCONFIG(TAG, "  Data pin %d: %s", i, (this->data_pins_[i])->dump_summary().c_str());
 }
-
+void RpiDpiRgb::reset_display_() const {
+  if (this->reset_pin_ != nullptr) {
+    this->reset_pin_->setup();
+    this->reset_pin_->digital_write(false);
+    if (this->enable_pin_ != nullptr) {
+      this->enable_pin_->setup();
+      this->enable_pin_->digital_write(false);
+    }
+    delay(1);
+    this->reset_pin_->digital_write(true);
+    if (this->enable_pin_ != nullptr) {
+      delay(11);
+      this->enable_pin_->digital_write(true);
+    }
+  }
+}
 }  // namespace rpi_dpi_rgb
 }  // namespace esphome
 
