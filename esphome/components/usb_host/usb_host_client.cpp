fe4b1305bcb05f57dbdfae90304a83268946e083
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-12-16 20:08:45 +1100

    Logging improvements

--- esphome/components/usb_host/usb_host_client.cpp
+++ esphome/components/usb_host/usb_host_client.cpp
@@ -8,8 +8,121 @@
 namespace esphome {
 namespace usb_host {
 
+#pragma GCC diagnostic ignored "-Wparentheses"
+
 using namespace bytebuffer;
 
+#if ESPHOME_LOG_LEVEL >= ESPHOME_LOG_LEVEL_VERBOSE
+static void print_ep_desc(const usb_ep_desc_t *ep_desc) {
+  const char *ep_type_str;
+  int type = ep_desc->bmAttributes & USB_BM_ATTRIBUTES_XFERTYPE_MASK;
+
+  switch (type) {
+    case USB_BM_ATTRIBUTES_XFER_CONTROL:
+      ep_type_str = "CTRL";
+      break;
+    case USB_BM_ATTRIBUTES_XFER_ISOC:
+      ep_type_str = "ISOC";
+      break;
+    case USB_BM_ATTRIBUTES_XFER_BULK:
+      ep_type_str = "BULK";
+      break;
+    case USB_BM_ATTRIBUTES_XFER_INT:
+      ep_type_str = "INT";
+      break;
+    default:
+      ep_type_str = NULL;
+      break;
+  }
+
+  ESP_LOGV(TAG, "\t\t*** Endpoint descriptor ***");
+  ESP_LOGV(TAG, "\t\tbLength %d", ep_desc->bLength);
+  ESP_LOGV(TAG, "\t\tbDescriptorType %d", ep_desc->bDescriptorType);
+  ESP_LOGV(TAG, "\t\tbEndpointAddress 0x%x\tEP %d %s", ep_desc->bEndpointAddress, USB_EP_DESC_GET_EP_NUM(ep_desc),
+           USB_EP_DESC_GET_EP_DIR(ep_desc) ? "IN" : "OUT");
+  ESP_LOGV(TAG, "\t\tbmAttributes 0x%x\t%s", ep_desc->bmAttributes, ep_type_str);
+  ESP_LOGV(TAG, "\t\twMaxPacketSize %d", ep_desc->wMaxPacketSize);
+  ESP_LOGV(TAG, "\t\tbInterval %d", ep_desc->bInterval);
+}
+
+static void usbh_print_intf_desc(const usb_intf_desc_t *intf_desc) {
+  ESP_LOGV(TAG, "\t*** Interface descriptor ***");
+  ESP_LOGV(TAG, "\tbLength %d", intf_desc->bLength);
+  ESP_LOGV(TAG, "\tbDescriptorType %d", intf_desc->bDescriptorType);
+  ESP_LOGV(TAG, "\tbInterfaceNumber %d", intf_desc->bInterfaceNumber);
+  ESP_LOGV(TAG, "\tbAlternateSetting %d", intf_desc->bAlternateSetting);
+  ESP_LOGV(TAG, "\tbNumEndpoints %d", intf_desc->bNumEndpoints);
+  ESP_LOGV(TAG, "\tbInterfaceClass 0x%x", intf_desc->bInterfaceProtocol);
+  ESP_LOGV(TAG, "\tiInterface %d", intf_desc->iInterface);
+}
+
+static void usbh_print_cfg_desc(const usb_config_desc_t *cfg_desc) {
+  ESP_LOGV(TAG, "*** Configuration descriptor ***");
+  ESP_LOGV(TAG, "bLength %d", cfg_desc->bLength);
+  ESP_LOGV(TAG, "bDescriptorType %d", cfg_desc->bDescriptorType);
+  ESP_LOGV(TAG, "wTotalLength %d", cfg_desc->wTotalLength);
+  ESP_LOGV(TAG, "bNumInterfaces %d", cfg_desc->bNumInterfaces);
+  ESP_LOGV(TAG, "bConfigurationValue %d", cfg_desc->bConfigurationValue);
+  ESP_LOGV(TAG, "iConfiguration %d", cfg_desc->iConfiguration);
+  ESP_LOGV(TAG, "bmAttributes 0x%x", cfg_desc->bmAttributes);
+  ESP_LOGV(TAG, "bMaxPower %dmA", cfg_desc->bMaxPower * 2);
+}
+
+void usb_client_print_device_descriptor(const usb_device_desc_t *devc_desc) {
+  if (devc_desc == NULL) {
+    return;
+  }
+
+  ESP_LOGV(TAG, "*** Device descriptor ***");
+  ESP_LOGV(TAG, "bLength %d", devc_desc->bLength);
+  ESP_LOGV(TAG, "bDescriptorType %d", devc_desc->bDescriptorType);
+  ESP_LOGV(TAG, "bcdUSB %d.%d0", ((devc_desc->bcdUSB >> 8) & 0xF), ((devc_desc->bcdUSB >> 4) & 0xF));
+  ESP_LOGV(TAG, "bDeviceClass 0x%x", devc_desc->bDeviceClass);
+  ESP_LOGV(TAG, "bDeviceSubClass 0x%x", devc_desc->bDeviceSubClass);
+  ESP_LOGV(TAG, "bDeviceProtocol 0x%x", devc_desc->bDeviceProtocol);
+  ESP_LOGV(TAG, "bMaxPacketSize0 %d", devc_desc->bMaxPacketSize0);
+  ESP_LOGV(TAG, "idVendor 0x%x", devc_desc->idVendor);
+  ESP_LOGV(TAG, "idProduct 0x%x", devc_desc->idProduct);
+  ESP_LOGV(TAG, "bcdDevice %d.%d0", ((devc_desc->bcdDevice >> 8) & 0xF), ((devc_desc->bcdDevice >> 4) & 0xF));
+  ESP_LOGV(TAG, "iManufacturer %d", devc_desc->iManufacturer);
+  ESP_LOGV(TAG, "iProduct %d", devc_desc->iProduct);
+  ESP_LOGV(TAG, "iSerialNumber %d", devc_desc->iSerialNumber);
+  ESP_LOGV(TAG, "bNumConfigurations %d", devc_desc->bNumConfigurations);
+}
+
+void usb_client_print_config_descriptor(const usb_config_desc_t *cfg_desc,
+                                        print_class_descriptor_cb class_specific_cb) {
+  if (cfg_desc == nullptr) {
+    return;
+  }
+
+  int offset = 0;
+  uint16_t wTotalLength = cfg_desc->wTotalLength;
+  const usb_standard_desc_t *next_desc = (const usb_standard_desc_t *) cfg_desc;
+
+  do {
+    switch (next_desc->bDescriptorType) {
+      case USB_W_VALUE_DT_CONFIG:
+        usbh_print_cfg_desc((const usb_config_desc_t *) next_desc);
+        break;
+      case USB_W_VALUE_DT_INTERFACE:
+        usbh_print_intf_desc((const usb_intf_desc_t *) next_desc);
+        break;
+      case USB_W_VALUE_DT_ENDPOINT:
+        print_ep_desc((const usb_ep_desc_t *) next_desc);
+        break;
+      default:
+        if (class_specific_cb) {
+          class_specific_cb(next_desc);
+        }
+        break;
+    }
+
+    next_desc = usb_parse_next_descriptor(next_desc, wTotalLength, &offset);
+
+  } while (next_desc != NULL);
+}
+#endif
 static std::string get_descriptor_string(const usb_str_desc_t *desc) {
   char buffer[256];
   if (desc == nullptr)
@@ -78,7 +191,7 @@ void USBClient::loop() {
         this->disconnect_();
       } else {
         ESP_LOGD(TAG, "Device descriptor: vid %X pid %X", desc->idVendor, desc->idProduct);
-        if (desc->idVendor == this->vid_ && desc->idProduct == this->pid_) {
+        if (desc->idVendor == this->vid_ && desc->idProduct == this->pid_ || this->vid_ == 0 && this->pid_ == 0) {
           usb_device_info_t dev_info;
           if ((err = usb_host_device_info(this->device_handle_, &dev_info)) != ESP_OK) {
             ESP_LOGW(TAG, "Device info failed: %s", esp_err_to_name(err));
@@ -95,11 +208,11 @@ void USBClient::loop() {
           const usb_device_desc_t *device_desc;
           err = usb_host_get_device_descriptor(this->device_handle_, &device_desc);
           if (err == ESP_OK)
-            usb_print_device_descriptor(device_desc);
+            usb_client_print_device_descriptor(device_desc);
           const usb_config_desc_t *config_desc;
           err = usb_host_get_active_config_descriptor(this->device_handle_, &config_desc);
           if (err == ESP_OK)
-            usb_print_config_descriptor(config_desc, nullptr);
+            usb_client_print_config_descriptor(config_desc, nullptr);
 #endif
           this->on_connected_();
         } else {
