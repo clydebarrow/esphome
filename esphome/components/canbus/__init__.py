e33d4c55e448e1895cde51174d4dcffdfb30a7b1
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-03-26 11:00:13 +1100

    test

--- esphome/components/canbus/__init__.py
+++ esphome/components/canbus/__init__.py
@@ -1,7 +1,6 @@
 from esphome import automation
 import esphome.codegen as cg
 
-
 import esphome.config_validation as cv
 from esphome.const import CONF_DATA, CONF_ID, CONF_TRIGGER_ID
 from esphome.core import CORE
