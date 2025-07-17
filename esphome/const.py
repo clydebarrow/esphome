17a5d4f984b4971cd7bb19bafacd7a28b1db713d
clydeps <U5yx99dok9>
2025-07-17 16:23:53 +1000

    testing

--- esphome/const.py
+++ esphome/const.py
@@ -1313,8 +1313,6 @@ KEY_PAST_SAFE_MODE = "past_safe_mode"
 
 ENTITY_CATEGORY_NONE = ""
 
-
-# The entity category for configuration values/controls
 ENTITY_CATEGORY_CONFIG = "config"
 
 # The entity category for read only diagnostic values, for example RSSI, uptime or MAC Address
