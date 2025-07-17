fefd993bc90aafac590a14fe388f9087f5a74c13
clydeps <U5yx99dok9>
2025-07-17 21:13:46 +1000

    Core

--- esphome/const.py
+++ esphome/const.py
@@ -1311,10 +1311,7 @@ KEY_NAME = "name"
 KEY_VARIANT = "variant"
 KEY_PAST_SAFE_MODE = "past_safe_mode"
 
-# Entity categories
 ENTITY_CATEGORY_NONE = ""
-
-# The entity category for configuration values/controls
 ENTITY_CATEGORY_CONFIG = "config"
 
 # The entity category for read only diagnostic values, for example RSSI, uptime or MAC Address
