0dd6f5f2d5af127a0bed271f34e0b706871a0cc0
clydeps <U5yx99dok9>
2025-07-17 16:42:45 +1000

    test

--- esphome/components/mipi_spi/display.py
+++ esphome/components/mipi_spi/display.py
@@ -621,4 +621,5 @@ async def to_code(config):
         cg.add(var.set_writer(lambda_))
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
+    # Displays are write-only, set the SPI device to write-only as well
     cg.add(var.set_write_only(True))
