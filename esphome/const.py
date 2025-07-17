9eac0b0440af11429f81bc3a80f4d9da02beb5c7
clydeps <U5yx99dok9>
2025-07-17 21:04:09 +1000

    testing core change

--- esphome/const.py
+++ esphome/const.py
@@ -1311,11 +1311,6 @@ KEY_NAME = "name"
 KEY_VARIANT = "variant"
 KEY_PAST_SAFE_MODE = "past_safe_mode"
 
-# Entity categories
 ENTITY_CATEGORY_NONE = ""
-
-# The entity category for configuration values/controls
 ENTITY_CATEGORY_CONFIG = "config"
-
-# The entity category for read only diagnostic values, for example RSSI, uptime or MAC Address
 ENTITY_CATEGORY_DIAGNOSTIC = "diagnostic"
