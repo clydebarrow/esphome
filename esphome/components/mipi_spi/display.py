bd2f62965d66cb8426047de098e3ac5179d1890b
clydeps <U5yx99dok9>
2025-07-17 16:07:36 +1000

    test pr

--- esphome/components/mipi_spi/display.py
+++ esphome/components/mipi_spi/display.py
@@ -621,4 +621,5 @@ async def to_code(config):
         cg.add(var.set_writer(lambda_))
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
+
     cg.add(var.set_write_only(True))
